b683ab69838c5831ea4487bd1415ce52c7e55ac7
IvanSlivenko <ivan8822@ukr.net>
2025-08-16 11:01:06 +0300

    16-08-2025-11-01

---
+++ start-template-shop/js/constants.js
@@ -0,0 +1,11 @@
+// Кількість карток при першому рендері
+export const COUNT_SHOW_CARDS_CLICK = 5;
+
+export const ERROR_SERVER = 'Помилка сервера, спробуйте пізніше!';
+export const NO_PRODUCTS_IN_THIS_CATEGORY = 'Товарів в цій категорії немає!';
+export const PRODUCT_INFORMATION_NOT_FOUND = 'Інформація про товар не знайдена!';
+export const NO_ITEMS_CART = 'В корзині немає товарів!';
+
+
+
+
