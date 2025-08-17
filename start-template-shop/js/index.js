4a33b1d2b9f69d8215655ab7bea2b4a9392ca3af
IvanSlivenko <ivan8822@ukr.net>
2025-08-17 12:11:52 +0300

    17-08-2025-12-11

--- start-template-shop/js/index.js
+++ start-template-shop/js/index.js
@@ -1,6 +1,6 @@
 "use strict"
 //==========================================
-import { 
+import {
     showErrorMessage,
     setBasketLocalStorage,
     getBasketLocalStorage,
@@ -8,8 +8,8 @@ import {
     formatPrice
 } from './utils.js';
 
-import { 
-    COUNT_SHOW_CARDS_CLICK, 
+import {
+    COUNT_SHOW_CARDS_CLICK,
     ERROR_SERVER,
     NO_PRODUCTS_IN_THIS_CATEGORY
 } from './constants.js';
@@ -26,44 +26,75 @@ getProducts();
 btnShowCards.addEventListener('click', scliceArrCards);
 cards.addEventListener('click', handleCartClick);
 
-async  function getProducts(){
+async function getProducts() {
     try {
-        if(!productsData.length){
+        if (!productsData.length) {
             const res = await fetch('./data/products.json');
-            if(!res.ok){
+            if (!res.ok) {
                 throw new Error(res.statusText);
             }
             productsData = await res.json();
-        
+
         }
 
         console.log(productsData);
 
-        if((productsData.length > COUNT_SHOW_CARDS_CLICK) && 
-            btnShowCards.classList.contains('none')){
-                btnShowCards.classList.remove('none');
-            }
+        if ((productsData.length > COUNT_SHOW_CARDS_CLICK) &&
+            btnShowCards.classList.contains('none')) {
+            btnShowCards.classList.remove('none');
+        }
 
-            renderStartPage(productsData);
+        renderStartPage(productsData);
 
     } catch (err) {
         showErrorMessage(ERROR_SERVER);
         console.log(err.message);
     }
-} 
+}
 
-function renderStartPage(data){
-    if(!data || !data.length){
+function checkingCountBasket() {
+    const bascket = getBasketLocalStorage();
+    const countBasket = document.querySelector('.basket__count');
+
+    if (!countBasket) return; // захист від null
+
+    if (!bascket.length) {
+        countBasket.classList.add('basket__count__zero');
+        // countBasket.textContent = bascket.length; // показати число
+    } else { countBasket.classList.remove('basket__count__zero'); }
+}
+
+function checkingActiveButtons(bascket) {
+    const buttons = document.querySelectorAll('.card__add');
+    buttons.forEach(btn => {
+        const card = btn.closest('.card');
+        const id = card.dataset.productId;
+        const isInBasket = bascket.includes(id);
+
+        btn.disabled = isInBasket;
+        btn.classList.toggle('active', isInBasket);
+        btn.textContent = isInBasket ? 'У Кошику' : 'До кошика';
+    });
+}
+
+function renderStartPage(data) {
+    if (!data || !data.length) {
         showErrorMessage(NO_PRODUCTS_IN_THIS_CATEGORY);
         return
     };
 
-    const arrCards = data.slice(0,COUNT_SHOW_CARDS_CLICK);
+    const arrCards = data.slice(0, COUNT_SHOW_CARDS_CLICK);
     createCards(arrCards);
+
+    checkingRelevanceValueBasket(data);
+
+    const bascket = getBasketLocalStorage();
+    checkingActiveButtons(bascket);
+    checkingCountBasket();
 }
 
-function scliceArrCards(){
-    if(shownCards >= productsData.length) return;
+function scliceArrCards() {
+    if (shownCards >= productsData.length) return;
 
     countClickBtnShowCards++;
     const countShowCards = COUNT_SHOW_CARDS_CLICK * countClickBtnShowCards;
@@ -72,29 +103,30 @@ function scliceArrCards(){
     createCards(arrCards);
     shownCards = cards.children.length;
 
-    if(shownCards >= productsData.length){
+    if (shownCards >= productsData.length) {
         btnShowCards.classList.add('none');
     }
 }
 
-function handleCartClick(event){
+function handleCartClick(event) {
     const targetButton = event.target.closest('.card__add');
-    if(!targetButton) return;
+    if (!targetButton) return;
 
     const card = targetButton.closest('.card');
     const id = card.dataset.productId;
-    const bascket =  getBasketLocalStorage();
+    const bascket = getBasketLocalStorage();
 
-    if(bascket.inclides(id))return;
+    if (bascket.includes(id)) return;
     bascket.push(id);
     setBasketLocalStorage(bascket);
-    chekingActiveButtons(bascket);
+    checkingActiveButtons(bascket);
+    console.log('checkingActiveButtons')
+    checkingCountBasket();
+    console.log('checkingCountBasket')
 
 }
 
-function chekingActiveButtons(){
 
-}
 
 
 
@@ -103,8 +135,8 @@ function createCards(data) {
     data.forEach(card => {
         const { id, img, title, price, discount } = card;
         const priceDiscount = price - ((price * discount) / 100);
-		const cardItem = 
-			`
+        const cardItem =
+            `
                 <div class="card" data-product-id="${id}">
                     <div class="card__top">
                         <a href="/card.html?id=${id}" class="card__image">
@@ -126,7 +158,8 @@ function createCards(data) {
                 </div>
             `
         cards.insertAdjacentHTML('beforeend', cardItem);
-	});
+    });
+
 }
 
 
